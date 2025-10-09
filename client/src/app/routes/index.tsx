8e46d9058745aa94f0e601f325d072c54566ce4b
038bkn <038bkn@gmail.com>
2025-10-09 11:08:12 +0900

    feat: リクエストページのルーティング設定

---
+++ client/src/app/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RequestPage } from "../../features/requests/pages/RequestPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <RequestPage />,
+    },
+]);
+
+export const AppRoutes = () => {
+    return <RouterProvider router={router} />;
+};

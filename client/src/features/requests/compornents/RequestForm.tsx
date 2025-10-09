7367c2ab10d2d875cfbbbee2c99cb846a6ada331
038bkn <038bkn@gmail.com>
2025-10-09 15:00:02 +0900

    feat: 申請画面UI実装

--- client/src/features/requests/compornents/RequestForm.tsx
+++ client/src/features/requests/compornents/RequestForm.tsx
@@ -1,5 +1,49 @@
+import { useEffect, useRef, useState } from "react"
+
 export const RequestForm = () => {
+
+    const [amount, setAmount] = useState("");
+    const [reason, setReason] = useState("");
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    useEffect(() => {
+        const textarea = textareaRef.current;
+        if (!textarea) return;
+
+        textarea.style.height = "auto";
+        textarea.style.height = textarea.scrollHeight + "px";
+    }, [reason]);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log("申請内容：", { amount, reason });
+    }
+
     return (
-        <div>フォーム</div>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+            <label className="flex flex-col gap-1">
+                <span className="text-sm font-semibold text-[valr(--color-text)">金額(円)</span>
+                <input
+                    type="number"
+                    placeholder="例：500"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                />
+            </label>
+
+            <label className="flex flex-col gap-1">
+                <span className="text-sm font-semibold text-[valr(--color-text)">理由</span>
+                <textarea
+                    ref={textareaRef}
+                    placeholder="例：友達とカフェに行きたい"
+                    value={reason}
+                    onChange={(e) => setReason(e.target.value)}
+                    className="resize-none overflow-hidden"
+                />
+            </label>
+            <button type="submit" className="btn-green">
+                申請を送信
+            </button>
+        </form>
     )
 }
--- client/src/features/requests/pages/RequestPage.tsx
+++ client/src/features/requests/pages/RequestPage.tsx
@@ -2,9 +2,11 @@ import { RequestForm } from "../compornents/RequestForm"
 
 export const RequestPage = () => {
     return (
-        <div>
-            <h1>リクエストフォームだよ</h1>
-            <RequestForm />
+        <div className="min-h-screen flex flex-col items-center p-6">
+            <div className="card-white max-w-md">
+                <h1 className="text-xl font-bold mb-4">お小遣い申請</h1>
+                <RequestForm />
+            </div>
         </div>
     )
 }

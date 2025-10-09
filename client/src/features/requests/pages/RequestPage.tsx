8cc5d97900e5f7ed47f47d44965fa5f30db5eaa9
038bkn <038bkn@gmail.com>
2025-10-09 10:29:59 +0900

    Add: テスト用リクエストUI

---
+++ client/src/features/requests/pages/RequestPage.tsx
@@ -0,0 +1,10 @@
+import { RequestForm } from "../compornents/RequestForm"
+
+export const RequestPage = () => {
+    return (
+        <div>
+            <h1>リクエストフォームだよ</h1>
+            <RequestForm />
+        </div>
+    )
+}

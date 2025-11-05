b8a7aa7f66b8650df6ff472bc41faec0ce24c713
038bkn <038bkn@gmail.com>
2025-11-05 16:24:07 +0900

    fix: ホーム画面実装

--- client/src/app/routes/index.tsx
+++ client/src/app/routes/index.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ApprovalPage } from "../../features/approvals/pages/ApprovalPage";
 import { HistoryPage } from "../../features/history/pages/HistoryPage";
+import { ParentHome } from "../../features/home/pages/ParentHome";
 import { RequestPage } from "../../features/requests/pages/RequestPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <ParentHome />,
+  },
+  {
+    path: "/request",
     element: <RequestPage />,
   },
   {

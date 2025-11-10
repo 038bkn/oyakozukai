764892291d8e1dd311426cdaa2b18c975767d7aa
038bkn <038bkn@gmail.com>
2025-11-11 01:06:33 +0900

    chore: URL修正

--- client/src/features/history/api/getTransactions.ts
+++ client/src/features/history/api/getTransactions.ts
@@ -3,7 +3,7 @@ import { API_BASE_URL } from "../../../shared/apiBase";
 import type { Transaction } from "../../../shared/types/transaction";
 
 export async function getTransactions(): Promise<Transaction[]> {
-  const data = await apiFetch<Transaction[]>(`${API_BASE_URL}}/transactions`);
+  const data = await apiFetch<Transaction[]>(`${API_BASE_URL}/transactions`);
   return data.map((t) => ({
     ...t,
     amount: Number(t.amount),
